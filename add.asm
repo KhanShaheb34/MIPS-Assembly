5242a62cf7e6da2299724e49e7072a64e1b9a77f
Shakirul Hasan <theshk.me@gmail.com>
2019-03-03 14:03:33 +0600

    Addition added

---
+++ add.asm
@@ -0,0 +1,28 @@
+.data
+str2:	.asciiz "Input a number\n"
+str3:	.asciiz "Input another number:\n"
+str4:	.asciiz "Addition:\n"
+	.text
+	
+main:
+	li $v0, 4
+	la $a0, str2
+	syscall
+	
+	li $v0, 5
+	syscall
+	move $t1, $v0
+	
+	li $v0, 4
+	la $a0, str3
+	syscall
+	
+	li $v0, 5
+	syscall
+	move $t2, $v0
+	
+	add $t3, $t1, $t2
+	
+	li $v0, 1
+	la $a0, ($t3)
+	syscall
